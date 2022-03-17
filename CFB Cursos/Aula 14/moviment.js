1e07d3db39afc6629ad0e62c323c0006b3cf4292
AdemirRamos <ademirramos634@gmail.com>
2022-03-17 10:44:36 -0300

    Adição da Aula 14

---
+++ CFB Cursos/Aula 14/moviment.js
@@ -0,0 +1,40 @@
+class Moviment {
+    constructor(contexto, teclado) {
+        this.context = contexto
+        this.keyboard = teclado
+        this.y = 0
+        this.x = 0
+        this.velocidade = 1
+        this.lofi = new Image()
+        this.lofi.src = './Mídia/Lofi.jpg'
+
+        //Também poderia ser feito desta maneira: "this.lofi.onLoad".
+
+        this.lofi.addEventListener('load', ()=> {
+            desenhar()
+        })
+    }
+
+    gerenciar() {
+        if (this.keyboard.esquerda) {
+            this.x -= this.velocidade
+        }
+
+        if (this.keyboard.direita) {
+            this.x += this.velocidade
+        }
+
+        if (this.keyboard.cima) {
+            this.y -= this.velocidade
+        }
+
+        if (this.keyboard.baixo) {
+            this.y += this.velocidade
+        }
+    }
+
+    desenhar() {
+        gerenciar()
+        this.context.drawImage(this.lofi, this.x, this.y)
+    }
+}
